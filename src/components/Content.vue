b5eae64927c97abea3a8ff4f403e095386f12c79
pshop <paul@typeisnone.dev>
2026-04-20 14:31:12 +0200

    added api call and json parser for displaying the right balises in HTML

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -15,21 +15,32 @@
                 class="v-content__content__text-container__section"
             >
               <div class="v-content__content__text-container__section__content">
-                <p>
-                  Installés à Montmirail près de Neuchâtel depuis 2011, nous tenons une exploitation spécialisée dans
-                  les grandes cultures (maïs, soja, tournesol, blé d’automne). Attentifs à la qualité des produits ainsi
-                  qu’à l’environnement, notre production est labellisée Bourgeon de Bio Suisse. Pour entretenir la
-                  biodiversité de la région, nous avons aussi des vergers, des ruches et un élevage bovin Natura-Beef.
-                  Pour ces produits, nous privilégions la vente directe et les circuits courts, vous pourrez donc
-                  trouver chez nous des <em>fruits de saison</em>, du <em>miel</em> ainsi que de la <em>viande</em>.
-                  Cette année, nous proposons en plus différentes
-                  variétés de <em>courges</em> ainsi que des <em>noix</em>.
-                </p>
-                <p>
-                  Pour plus d’informations ou pour passer commande directement, vous pouvez nous joindre au 079 692 15
-                  18 ou nous écrire à <i>farm@doncamillo.ch</i>. Nous serons ravis de vous renseigner plus en
-                  détail et de vous accueillir à la ferme.
-                </p>
+                <template v-if="presentation">
+                  <p v-for="(block, index) in presentation.text_content" :key="index">
+                    <template v-for="(child, childIndex) in block.children" :key="childIndex">
+                      <em v-if="child.bold">{{ child.text }}</em>
+                      <i v-else-if="child.underline">{{ child.text }}</i>
+                      <template v-else>{{ child.text }}</template>
+                    </template>
+                  </p>
+                </template>
+                <div v-else>
+                  <p>
+                    Installés à Montmirail près de Neuchâtel depuis 2011, nous tenons une exploitation spécialisée dans
+                    les grandes cultures (maïs, soja, tournesol, blé d’automne). Attentifs à la qualité des produits ainsi
+                    qu’à l’environnement, notre production est labellisée Bourgeon de Bio Suisse. Pour entretenir la
+                    biodiversité de la région, nous avons aussi des vergers, des ruches et un élevage bovin Natura-Beef.
+                    Pour ces produits, nous privilégions la vente directe et les circuits courts, vous pourrez donc
+                    trouver chez nous des <em>fruits de saison</em>, du <em>miel</em> ainsi que de la <em>viande</em>.
+                    Cette année, nous proposons en plus différentes
+                    variétés de <em>courges</em> ainsi que des <em>noix</em>.
+                  </p>
+                  <p>
+                    Pour plus d’informations ou pour passer commande directement, vous pouvez nous joindre au 079 692 15
+                    18 ou nous écrire à <i>farm@doncamillo.ch</i>. Nous serons ravis de vous renseigner plus en
+                    détail et de vous accueillir à la ferme.
+                  </p>
+                </div>
               </div>
             </section>
 
@@ -38,18 +49,29 @@
                 class="v-content__content__text-container__section"
             >
               <div class="v-content__content__text-container__section__content">
-                <p>
-                  Nos vergers produisent une grande variété de fruits : les pommes Rubinette, Idared, Boskoop et Gala
-                  ainsi que les poires Beurré Bosc et Conférence. Pour prendre le relais de l’ancien verger, nous en
-                  avons crée depuis quelques années un nouveau avec les variétés de pommes qui ont fait leurs preuves,
-                  complété par la variété Topaz.
-                </p>
-                <p>
-                  En plus du verger classique, nous avons sur notre domaine un verger haute-tige composé
-                  essentiellement de variétés anciennes et robustes qui enrichissent le paysage et complètent la
-                  diversité végétale de la ferme. Outre les pommiers et les poiriers, on y trouve des pruniers et des
-                  mirabelliers, par exemple.
-                </p>
+                <template v-if="fruit">
+                  <p v-for="(block, index) in fruit.text_content" :key="index">
+                    <template v-for="(child, childIndex) in block.children" :key="childIndex">
+                      <em v-if="child.bold">{{ child.text }}</em>
+                      <i v-else-if="child.underline">{{ child.text }}</i>
+                      <template v-else>{{ child.text }}</template>
+                    </template>
+                  </p>
+                </template>
+                <template v-else>
+                  <p>
+                    Nos vergers produisent une grande variété de fruits : les pommes Rubinette, Idared, Boskoop et Gala
+                    ainsi que les poires Beurré Bosc et Conférence. Pour prendre le relais de l’ancien verger, nous en
+                    avons crée depuis quelques années un nouveau avec les variétés de pommes qui ont fait leurs preuves,
+                    complété par la variété Topaz.
+                  </p>
+                  <p>
+                    En plus du verger classique, nous avons sur notre domaine un verger haute-tige composé
+                    essentiellement de variétés anciennes et robustes qui enrichissent le paysage et complètent la
+                    diversité végétale de la ferme. Outre les pommiers et les poiriers, on y trouve des pruniers et des
+                    mirabelliers, par exemple.
+                  </p>
+                </template>
               </div>
             </section>
 
@@ -58,12 +80,23 @@
                 class="v-content__content__text-container__section"
             >
               <div class="v-content__content__text-container__section__content">
-                <p>
-                  En plus des fruits frais, nous proposons du <em>jus de pomme</em> et <em>des fruits séchés</em> qui
-                  permettent de déguster tout au long de l’année notre production estivale et automnale. Afin d’utiliser les fruits
-                  mûrs invendus, nous nous sommes aussi mis à produire de <em>l’eau-de-vie</em> en petite quantité,
-                  disponible actuellement en trois sortes différentes : Pomme Rubinette, Pruneau Fellenberg et Poire.
-                </p>
+                <template v-if="produit">
+                  <p v-for="(block, index) in produit.text_content" :key="index">
+                    <template v-for="(child, childIndex) in block.children" :key="childIndex">
+                      <em v-if="child.bold">{{ child.text }}</em>
+                      <i v-else-if="child.underline">{{ child.text }}</i>
+                      <template v-else>{{ child.text }}</template>
+                    </template>
+                  </p>
+                </template>
+                <template v-else>
+                  <p>
+                    En plus des fruits frais, nous proposons du <em>jus de pomme</em> et <em>des fruits séchés</em> qui
+                    permettent de déguster tout au long de l’année notre production estivale et automnale. Afin d’utiliser les fruits
+                    mûrs invendus, nous nous sommes aussi mis à produire de <em>l’eau-de-vie</em> en petite quantité,
+                    disponible actuellement en trois sortes différentes : Pomme Rubinette, Pruneau Fellenberg et Poire.
+                  </p>
+                </template>
               </div>
             </section>
 
@@ -72,15 +105,26 @@
                 class="v-content__content__text-container__section"
             >
               <div class="v-content__content__text-container__section__content">
-                <p>
-                  Parce que nous aimons la viande et qu’il est important pour nous d’en connaître la provenance ainsi
-                  que de prodiguer un élevage respectueux des animaux, nos vaches sont élevées d’après les normes du
-                  label Natura-Beef (voir <a href="https://www.mutterkuh.ch/fr/natura-beef" target="_blank">mutterkuh.ch</a>).
-                  La viande provient de jeunes bêtes qui grandissent dans des élevages de vaches mères. Les veaux sont
-                  allaités et restent près de leurs mères jusqu’à l’âge de 10 mois. Leur nutrition se compose essentiellement
-                  de lait maternel et de fourrages grossiers. Le&nbsp;troupeau pâture sur nos propres prés et passe l’hiver en stabulation libre.
-                  Les vaches et les veaux sont en compagnie de leur taureau.
-                </p>
+                <template v-if="natura">
+                  <p v-for="(block, index) in natura.text_content" :key="index">
+                    <template v-for="(child, childIndex) in block.children" :key="childIndex">
+                      <em v-if="child.bold">{{ child.text }}</em>
+                      <i v-else-if="child.underline">{{ child.text }}</i>
+                      <template v-else>{{ child.text }}</template>
+                    </template>
+                  </p>
+                </template>
+                <template v-else>
+                  <p>
+                    Parce que nous aimons la viande et qu’il est important pour nous d’en connaître la provenance ainsi
+                    que de prodiguer un élevage respectueux des animaux, nos vaches sont élevées d’après les normes du
+                    label Natura-Beef (voir <a href="https://www.mutterkuh.ch/fr/natura-beef" target="_blank">mutterkuh.ch</a>).
+                    La viande provient de jeunes bêtes qui grandissent dans des élevages de vaches mères. Les veaux sont
+                    allaités et restent près de leurs mères jusqu’à l’âge de 10 mois. Leur nutrition se compose essentiellement
+                    de lait maternel et de fourrages grossiers. Le&nbsp;troupeau pâture sur nos propres prés et passe l’hiver en stabulation libre.
+                    Les vaches et les veaux sont en compagnie de leur taureau.
+                  </p>
+                </template>
                 <div style="margin-top: 1rem">
                   <a href="/ferme_montmirail_naturabeef_2025.pdf" target="_blank" class="m-link-with-img">
                     <span>La liste des prix est par ici </span>
@@ -95,9 +139,20 @@
                 class="v-content__content__text-container__section"
             >
               <div class="v-content__content__text-container__section__content">
-                <p>
-                  Depuis 2018, Angela s’occupe également de quelques colonies d’abeilles qui contribuent à la pollinisation des arbres fruitiers et d’autres cultures en fleurs aux alentours de notre ferme. Nous récoltons le miel de printemps de fin mai à début juin et le miel d’été vers la mi-juillet. À travers ses saveurs, le miel reflète l’offre florale du moment (et la météo) dans toute sa diversité.
-                </p>
+                <template v-if="miel">
+                  <p v-for="(block, index) in miel.text_content" :key="index">
+                    <template v-for="(child, childIndex) in block.children" :key="childIndex">
+                      <em v-if="child.bold">{{ child.text }}</em>
+                      <i v-else-if="child.underline">{{ child.text }}</i>
+                      <template v-else>{{ child.text }}</template>
+                    </template>
+                  </p>
+                </template>
+                <template v-else>
+                  <p>
+                    Depuis 2018, Angela s’occupe également de quelques colonies d’abeilles qui contribuent à la pollinisation des arbres fruitiers et d’autres cultures en fleurs aux alentours de notre ferme. Nous récoltons le miel de printemps de fin mai à début juin et le miel d’été vers la mi-juillet. À travers ses saveurs, le miel reflète l’offre florale du moment (et la météo) dans toute sa diversité.
+                  </p>
+                </template>
               </div>
             </section>
           </div>
@@ -139,8 +194,16 @@
 <script setup lang="ts">
 import { onMounted, onUnmounted, ref, nextTick } from "vue"
 import { useGlobalState } from "@/stores/globalState"
+import { getMiel, getFruit, getNatura, getProduit, getPresentation } from "@/services/api";
 
 const globalState = useGlobalState()
+
+// Reactive data for API content
+const presentation = ref<any>(null)
+const fruit = ref<any>(null)
+const produit = ref<any>(null)
+const natura = ref<any>(null)
+const miel = ref<any>(null)
 
 // Template refs
 const textContainerEl = ref<HTMLElement | null>(null)
@@ -151,33 +214,37 @@ const imgGalleryEl = ref<HTMLElement | null>(null)
  * This ensures all text is visible on different screen sizes and aspect ratios.
  */
 const autoAdjustFontSize = () => {
-  requestAnimationFrame(() => {
-    if (!textContainerEl.value) return
+  if (!textContainerEl.value) return
 
-    const textViewHeight = textContainerEl.value.getBoundingClientRect().height
-    const sections = textContainerEl.value.querySelectorAll('.v-content__content__text-container__section')
+  const textViewHeight = textContainerEl.value.getBoundingClientRect().height
+  const sections = textContainerEl.value.querySelectorAll('.v-content__content__text-container__section')
 
-    let needsAdjustment = false
-    sections.forEach((section: any) => {
-      // If a single section is taller than the container, we shrink everything
-      if (section.scrollHeight > textViewHeight) {
-        needsAdjustment = true
+  let needsAdjustment = false
+  for (const section of sections) {
+    if (section.scrollHeight > textViewHeight) {
+      needsAdjustment = true
+      break
+    }
+  }
+
+  if (needsAdjustment) {
+    const contentElements = textContainerEl.value.querySelectorAll('.v-content__content__text-container__section__content *')
+    let canStillShrink = false
+    contentElements.forEach((el: any) => {
+      if (el instanceof HTMLElement) {
+        const currentSize = parseFloat(window.getComputedStyle(el).fontSize)
+        if (currentSize > 12) { // Minimum font size 12px
+          el.style.lineHeight = '1.3em'
+          el.style.fontSize = (currentSize - 0.2) + 'px'
+          canStillShrink = true
+        }
       }
     })
-
-    if (needsAdjustment) {
-      const contentElements = textContainerEl.value.querySelectorAll('.v-content__content__text-container__section__content *')
-      contentElements.forEach((el: any) => {
-        if (el instanceof HTMLElement) {
-          const currentSize = parseFloat(window.getComputedStyle(el).fontSize)
-          el.style.lineHeight = '1.3em'
-          // Recursively decrease font-size until it fits
-          el.style.fontSize = (currentSize - 0.1) + 'px'
-        }
-      })
-      autoAdjustFontSize()
+    
+    if (canStillShrink) {
+      requestAnimationFrame(autoAdjustFontSize)
     }
-  })
+  }
 }
 
 /**
@@ -229,7 +296,30 @@ const onScroll = (e: Event) => {
   }
 }
 
-onMounted(() => {
+onMounted(async () => {
+  try {
+    const [
+      { data: resProduit },
+      { data: resNatura },
+      { data: resMiel },
+      { data: resPresentation },
+      { data: resFruit }
+    ] = await Promise.all([
+      getProduit(),
+      getNatura(),
+      getMiel(),
+      getPresentation(),
+      getFruit()
+    ])
+    produit.value = resProduit.data
+    natura.value = resNatura.data
+    miel.value = resMiel.data
+    presentation.value = resPresentation.data
+    fruit.value = resFruit.data
+  } catch (error) {
+    console.error("Failed to fetch content:", error)
+  }
+
   nextTick(() => {
     autoAdjustFontSize()
     setupSectionObserver()
