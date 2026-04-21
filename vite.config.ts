368e9649095ca6b4e27c603c15a100a845544acf
pshop <paul@typeisnone.dev>
2026-04-21 14:27:11 +0200

    typeisnone.dev

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/montmirail.web/',
+  base: '',
   build: {
     outDir: 'docs'
   },
